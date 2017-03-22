0aab42acf4eb66c3c60bf74857c6b865c8c2bd08
askyel <ariel.s.levy1@gmail.com>
2017-03-22 10:08:08 -0400

    basic linecharts

---
+++ examples/example.jsx
@@ -0,0 +1,161 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import LineChart from "../src/index.jsx"
+
+
+class KeyValueRow extends React.Component {
+
+  changeHandlerX(e) {
+    this.props.updateData({
+      title: this.props.title,
+      x: e.target.value,
+      y: this.props.y
+    })
+  }
+
+  changeHandlerY(e) {
+    this.props.updateData({
+      title: this.props.title,
+      x: this.props.x,
+      y: e.target.value
+    })
+  }
+
+  render() {
+    const style = {
+      cell: {
+        minWidth: "100px",
+      }
+    }
+
+    return(
+      <tr key={this.props.title}>
+        <td style={style.cell}>{this.props.title} </td>
+        <td style={style.cell}>
+          <input type="text" value={parseInt(this.props.x)}
+            onChange={this.changeHandlerX.bind(this)} />
+        </td>
+        <td style={style.cell}>
+          <input type="text" value={parseInt(this.props.y)}
+            onChange={this.changeHandlerY.bind(this)} />
+        </td>
+      </tr>
+    )
+  }
+}
+
+
+class KeyValueTable extends React.Component {
+
+  render() {
+    const style = {
+      container: {
+        width:"30%",
+        display:"inline-block",
+        verticalAlign: "top",
+        padding: "20px",
+      }
+    }
+    let rows = []
+    for (let dataPoint of this.props.data) {
+      rows.push(
+        <KeyValueRow key={dataPoint[this.props.titleKey]+"-"+dataPoint[this.props.xKey]}
+          title={dataPoint[this.props.titleKey]} x={dataPoint[this.props.xKey]} y={dataPoint[this.props.yKey]}
+          updateData={this.props.updateData.bind(this)} />
+      )
+    }
+
+    return (
+      <div className="container" style={style.container}>
+        <table>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
+}
+
+
+class ExampleApp extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+    //   data: [
+    //     {location: "Global", year: 2010, population: 6916183482},
+    //     {location: "Global", year: 2011, population: 6997998760},
+    //     {location: "Global", year: 2012, population: 7080072417},
+    //     {location: "Global", year: 2013, population: 7162119434},
+    //     {location: "Global", year: 2014, population: 7243784121},
+    //     {location: "Global", year: 2015, population: 7324782225},
+    //     {location: "Global", year: 2016, population: 7404976783},
+    //     {location: "US", year: 2010, population: 310559000},
+    //     {location: "US", year: 2011, population: 312917100},
+    //     {location: "US", year: 2012, population: 315219700},
+    //     {location: "US", year: 2013, population: 317474000},
+    //     {location: "US", year: 2014, population: 319849000},
+    //     {location: "US", year: 2015, population: 322060100},
+    //     {location: "US", year: 2016, population: 324304400},
+    //     {location: "India", year: 2010, population: 1186000000},
+    //     {location: "India", year: 2011, population: 1210570000},
+    //     {location: "India", year: 2012, population: 1213370000},
+    //     {location: "India", year: 2013, population: 1223000000},
+    //     {location: "India", year: 2014, population: 1267000000},
+    //     {location: "India", year: 2015, population: 1283000000},
+    //     {location: "India", year: 2016, population: 1299000000},
+    //   ]
+    // }
+      data: [
+        {location: "Global", year: 2013, population: 10},
+        {location: "Global", year: 2014, population: 9},
+        {location: "Global", year: 2015, population: 8},
+        {location: "Global", year: 2016, population: 7},
+        {location: "US", year: 2010, population: 3},
+        {location: "US", year: 2011, population: 4},
+        {location: "US", year: 2012, population: 4},
+        {location: "US", year: 2013, population: 3},
+        {location: "India", year: 2005, population: 0},
+        {location: "India", year: 2006, population: 1},
+        {location: "India", year: 2007, population: 5},
+        {location: "India", year: 2008, population: 6},
+      ]
+    }
+  }
+
+  updateData(mutatedObject) {
+    let mutatedData = JSON.parse(JSON.stringify(this.state.data))
+    let chosenIndex = -1
+    for (let index=0; index < mutatedData.length; index++) {
+      if (mutatedData[index].title === mutatedObject.title && mutatedData[index].x == mutatedObject.x) {
+        chosenIndex = index
+        break
+      }
+    }
+    if (chosenIndex > -1) {
+      mutatedData[chosenIndex].x = parseFloat(mutatedObject.x)
+      mutatedData[chosenIndex].y = parseFloat(mutatedObject.y)
+      this.setState({data: mutatedData})
+    }
+  }
+
+  render() {
+    return(
+      <div className="container">
+        <h1 style={{textAlign: "center"}}> Ent: Linecharts for react </h1>
+        <KeyValueTable data={this.state.data} updateData={this.updateData.bind(this)} titleKey="location" xKey="year" yKey="population" />
+        <div style={{width:"70%", display:"inline-block"}}>
+          <LineChart data={this.state.data} titleKey="location" xKey="year" yKey="population"/>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+ReactDOM.render(
+  <ExampleApp />,
+  document.getElementById("react-app")
+)
---
+++ src/LineChart.jsx
@@ -0,0 +1,211 @@
+import React from "react"
+//import isLight from "./isLight.js"
+import {spring, Motion} from "react-motion"
+
+const palette = [
+  "#4cab92",
+  "#ca0004",
+  "#003953",
+  "#eccc00",
+  "#9dbd5f",
+  "#0097bf",
+  "#005c7a",
+  "#fc6000"
+]
+
+
+class Line extends React.Component {
+
+  render() {
+    return(
+      <Motion
+        defaultStyle={{
+          x1: 0,
+          y1: 0,
+          x2: 0,
+          y2: 0,
+        }}
+        style={{
+          x1: spring(this.props.x1, {stiffness: 120, damping: 26}),
+          y1: spring(this.props.y1, {stiffness: 120, damping: 26}),
+          x2: spring(this.props.x2, {stiffness: 120, damping: 26}),
+          y2: spring(this.props.y2, {stiffness: 120, damping: 26}),
+        }}
+      >
+        {
+          interpolatingStyles =>
+            <g>
+              <line
+                x1={interpolatingStyles.x1}
+                y1={interpolatingStyles.y1}
+                x2={interpolatingStyles.x2}
+                y2={interpolatingStyles.y2}
+                stroke={this.props.stroke} />
+            </g>
+        }
+      </Motion>
+    )
+  }
+
+}
+
+class LineSeries extends React.Component {
+
+  render() {
+    let lines = []
+    for (var i=0; i < this.props.numpoints-1; i++) {
+      lines.push(
+        <Line x1={this.props.points[i][0]} y1={this.props.points[i][1]}
+          x2={this.props.points[i+1][0]} y2={this.props.points[i+1][1]}
+          stroke={this.props.color} />
+      )
+    }
+
+    return(
+      <g>{lines}</g>
+    )
+  }
+
+}
+
+class LineChart extends React.Component {
+
+  render() {
+    let width = this.props.width
+    let height = this.props.height
+    let buffer = 75
+    let data = JSON.parse(JSON.stringify(this.props.data))
+    let titleKey = this.props.titleKey
+    let xKey = this.props.xKey
+    let yKey = this.props.yKey
+    let xvals = data.map(function(d) {return parseFloat(d[xKey])})
+    let yvals = data.map(function(d) {return parseFloat(d[yKey])})
+    let maxX = Math.max.apply(Math, xvals)
+    let minX = Math.min.apply(Math, xvals)
+    let maxY = Math.max.apply(Math, yvals)
+    let minY = Math.min.apply(Math, yvals)
+
+    let chartWidth = width - 2*buffer
+    let chartHeight = height - 2*buffer
+    let chartX = buffer
+    let chartY = buffer
+
+    let sets = []
+    let setTitles = []
+    for (let member of data) {
+      let key = setTitles.indexOf(member[titleKey])
+
+      if (key != -1) {
+        sets[key].push([parseFloat(member[xKey]), parseFloat(member[yKey])])
+      } else {
+        setTitles.push(member[titleKey])
+        sets.push([[parseFloat(member[xKey]), parseFloat(member[yKey])]])
+      }
+    }
+
+    let series = []
+    let numsets = setTitles.length
+    for (var i=0; i < numsets; i++) {
+      sets[i].sort(function(a, b) {
+        return a[0] - b[0]
+      })
+
+      // extend lines on left
+      if (sets[i][0][0] > minX) {
+        let slope = (sets[i][1][1] - sets[i][0][1])/(sets[i][1][0] - sets[i][0][0])
+        let newY = sets[i][0][1] - slope * (sets[i][0][0] - minX)
+        sets[i].splice(0,0,[minX, newY])
+        if (newY > maxY) {
+          maxY = newY
+        } else if (newY < minY) {
+          minY = newY
+        }
+      }
+
+      // extend lines on right
+      let last = sets[i].length - 1
+      if (sets[i][last][0] < maxX) {
+        let slope = (sets[i][last][1] - sets[i][last-1][1])/(sets[i][last][0] - sets[i][last-1][0])
+        let newY = sets[i][last][1] + slope * (maxX - sets[i][last][0])
+        sets[i].push([maxX, newY])
+        if (newY > maxY) {
+          maxY = newY
+        } else if (newY < minY) {
+          minY = newY
+        }
+      }
+
+      for (let member of sets[i]) {
+        member[0] = (parseFloat(member[0])-minX) * (chartWidth) / (maxX-minX) + chartX
+        member[1] = (chartHeight) - (parseFloat(member[1])-minY) * (chartHeight) / (maxY-minY) + chartY
+      }
+
+      series.push(
+        <LineSeries points={sets[i]} numpoints={sets[i].length} color={palette[i%palette.length]} />
+      )
+    }
+
+    // x-axis
+    series.push(
+      <Line x1={chartX} y1={chartY+chartHeight}
+        x2={chartX+chartWidth} y2={chartY+chartHeight}
+        stroke={"rgb(0,0,0)"} />
+
+    )
+    let xTicks = Math.round((width-buffer)/50) + 1
+    if (xTicks < 5) {
+      xTicks = 5
+    }
+    let xSpace = (chartWidth) / (xTicks-1)
+    for (var j=0; j < xTicks; j++) {
+      series.push(
+        <Line x1={chartX+j*xSpace} y1={chartY+chartHeight}
+          x2={chartX+j*xSpace} y2={chartY+chartHeight+buffer*0.2}
+          stroke={"rgb(0,0,0)"} />
+      )
+      series.push(
+        <text x={chartX+j*xSpace} y={chartY+chartHeight+buffer*0.2} fontSize={buffer/5} style={{"writing-mode": "tb"}}>
+        {(minX + j*xSpace*(maxX-minX)/(chartWidth)).toFixed(2)}
+      </text>
+      )
+    }
+
+    // y-axis
+    series.push(
+      <Line x1={chartX} y1={chartY+chartHeight}
+        x2={chartX} y2={chartY}
+        stroke={"rgb(0,0,0)"} />
+    )
+    let yTicks = Math.round((chartHeight)/50) + 1
+    if (yTicks < 5) {
+      yTicks = 5
+    }
+    let ySpace = (chartHeight) / (yTicks-1)
+    for (var k=0; k < yTicks; k++) {
+      series.push(
+        <Line x1={chartX} y1={chartHeight+chartY-k*ySpace}
+          x2={chartX-buffer*0.2} y2={chartHeight+chartY-k*ySpace}
+          stroke={"rgb(0,0,0)"} />
+      )
+      series.push(
+        <text x={0} y={chartHeight+chartY-k*ySpace+buffer/10} fontSize={buffer/5}>
+        {(minY + k*ySpace*(maxY-minY)/(chartHeight)).toFixed(2)}
+       </text>
+     )
+    }
+
+    return(
+      <svg width={width} height={height}>
+        {series}
+      </svg>
+    )
+  }
+
+}
+
+LineChart.defaultProps = {
+  width: 800,
+  height: 400,
+}
+
+export default LineChart
