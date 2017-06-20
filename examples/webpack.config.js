14bdb565f5af1fe942e503a8785d69034e836901
Matt Farejowicz <mfarejowicz@gmail.com>
2017-06-20 14:52:57 -0400

    url fixes from react-line to replot-line

--- examples/webpack.config.js
+++ examples/webpack.config.js
@@ -1,18 +1,20 @@
-module.exports = {
-  entry: "./example.jsx",
-  output: {
-    path: "static",
-    filename: "bundle.js"
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.jsx$/,
-        loader: "babel-loader",
-        query: {
-          "presets": ["es2015", "react"]
+module.exports = function() {
+  return {
+    entry: "./example.jsx",
+    output: {
+      path: __dirname + "/static",
+      filename: "bundle.js"
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx$/,
+          loader: "babel-loader",
+          query: {
+            "presets": ["es2015", "react"]
+          }
         }
-      }
-    ]
+      ]
+    }
   }
 };
